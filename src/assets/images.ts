18a51f1e9e5f4afda20638b3414be5cb39307102
Noah <seohyun060@naver.com>
2023-09-27 08:43:13 +0900

    image

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -4,18 +4,6 @@ const images = {
 	search: '/assets/images/search.svg',
 	trash: '/assets/images/trash.svg',
 	back: '/assets/images/back.svg',
-	home: '/assets/images/home.svg',
-	researcher: '/assets/images/researcher.svg',
-	event: '/assets/images/event.svg',
-	project: '/assets/images/project.svg',
-	insight: '/assets/images/insight.svg',
-	newsletter: '/assets/images/newsletter.svg',
-	orcagroup: '/assets/images/orcagroup.svg',
-	up_w: '/assets/images/up-w.svg',
-	down_w: '/assets/images/down-w.svg',
-	down_b: '/assets/images/down-b.svg',
-	download: '/assets/images/download.svg',
-	add: '/assets/images/add.svg',
 };
 
 export default images;
