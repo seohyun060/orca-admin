24710adb8a9acd126facf42263d14a624e4d4d01
Noah <seohyun060@naver.com>
2023-10-25 15:32:37 +0900

    home 데이터 연동

--- src/api/projectsAPI.jsx
+++ src/api/projectsAPI.jsx
@@ -1,209 +1,228 @@
-import client from "./client.js";
+import client from './client.js';
 
 export async function getAllProjectData() {
-  return fetch(`http://43.202.46.227/api/projects`, {
-    method: "GET",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`http://43.202.46.227/api/projects`, {
+		method: 'GET',
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
 }
-
+export async function getMainProjects() {
+	return fetch(`http://43.202.46.227/api/projects?select=true`, {
+		method: 'GET',
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
+}
+export async function putMainProjects(urls) {
+	let data = new FormData();
+	console.log(urls);
+	data.append('urls', urls);
+	return fetch(`http://43.202.46.227/api/projects`, {
+		method: 'PUT',
+		body: data, // 데이터를 JSON 문자열로 변환하여 본문에 추가
+	})
+		.then((res) => res.json())
+		.then((data) => {});
+}
 export async function getOneProjectData(id) {
-  return fetch(`http://43.202.46.227/api/projects/${id}`, {
-    method: "GET",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`http://43.202.46.227/api/projects/${id}`, {
+		method: 'GET',
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
 }
 
 export async function postNewProjectData(data) {
-  const jsonData = JSON.stringify(data);
+	const jsonData = JSON.stringify(data);
 
-  return fetch(`http://43.202.46.227/api/projects`, {
-    method: "POST",
-    body: jsonData,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`http://43.202.46.227/api/projects`, {
+		method: 'POST',
+		body: jsonData,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
 }
 
 export async function putChangeStoredProjectData(id, isStored) {
-  const jsonData = JSON.stringify([]);
+	const jsonData = JSON.stringify([]);
 
-  return fetch(`http://43.202.46.227/api/projects/${id}?store=${isStored}`, {
-    method: "PUT",
-    // body: jsonData,
-    // headers: {
-    //   "Content-Type": "application/json",
-    // },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`http://43.202.46.227/api/projects/${id}?store=${isStored}`, {
+		method: 'PUT',
+		// body: jsonData,
+		// headers: {
+		//   "Content-Type": "application/json",
+		// },
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
 }
 
 export async function putOneProjectData(id, data) {
-  const jsonData = JSON.stringify(data);
+	const jsonData = JSON.stringify(data);
 
-  return fetch(`http://43.202.46.227/api/projects/${id}`, {
-    method: "PUT",
-    body: jsonData,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`http://43.202.46.227/api/projects/${id}`, {
+		method: 'PUT',
+		body: jsonData,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
 }
 
 export async function deleteOneProjectData(id) {
-  return fetch(`http://43.202.46.227/api/projects/${id}`, {
-    method: "DELETE",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
+	return fetch(`http://43.202.46.227/api/projects/${id}`, {
+		method: 'DELETE',
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			return data;
+		});
 }
 
 export const testAPI = async () => {
-  let data = new FormData();
-  let formDate = new FormData();
+	let data = new FormData();
+	let formDate = new FormData();
 
-  // formData.append("projectId", "2.13454835226356");
-  // formData.append("status", "ACTIVE");
-  // formData.append(
-  //   "projectTitle",
-  //   "Real-time Decision Support by Light-weighted AI Model Trained with Large-scale Data for Breast Cancer 30"
-  // );
-  // formData.append("startDate", "2023-05-22");
-  // formData.append("completeDate", "2025-05-22");
-  // formData.append("enrollment", 2000);
-  // formData.append("studyType", "CHAT_AI");
-  // formData.append("otherStudyId", null);
-  // formData.append(
-  //   "overview",
-  //   "Lorem ipsum dolor sit amet, cosectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, cosectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation"
-  // );
-  // formData.append("officialTitle", null);
-  // formData.append("conditions", "Myocardial");
-  // formData.append("name", null);
-  // formData.append("phoneNumber", null);
-  // formData.append("email", null);
-  // formData.append("location", "Bukgu, Daegu, Korea");
-  // formData.append("inclusionCriteria", null);
-  // formData.append("exclusionCriteria", null);
-  // formData.append("ageEligible", null);
-  // formData.append("sexEligible", "ALL");
-  // formData.append("acceptedHealthy", "YES");
-  // formData.append("samplingMethod", null);
-  // formData.append("observationalModel", null);
-  // formData.append("timePerspective", null);
-  // formData.append("interventionTreatment", null);
-  // formData.append("primaryOutcome", null);
-  // formData.append("secondaryOutcome", null);
-  // formData.append("pi", null);
-  // formData.append("collaborators", null);
-  // formData.append("publications", null);
+	// formData.append("projectId", "2.13454835226356");
+	// formData.append("status", "ACTIVE");
+	// formData.append(
+	//   "projectTitle",
+	//   "Real-time Decision Support by Light-weighted AI Model Trained with Large-scale Data for Breast Cancer 30"
+	// );
+	// formData.append("startDate", "2023-05-22");
+	// formData.append("completeDate", "2025-05-22");
+	// formData.append("enrollment", 2000);
+	// formData.append("studyType", "CHAT_AI");
+	// formData.append("otherStudyId", null);
+	// formData.append(
+	//   "overview",
+	//   "Lorem ipsum dolor sit amet, cosectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, cosectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation"
+	// );
+	// formData.append("officialTitle", null);
+	// formData.append("conditions", "Myocardial");
+	// formData.append("name", null);
+	// formData.append("phoneNumber", null);
+	// formData.append("email", null);
+	// formData.append("location", "Bukgu, Daegu, Korea");
+	// formData.append("inclusionCriteria", null);
+	// formData.append("exclusionCriteria", null);
+	// formData.append("ageEligible", null);
+	// formData.append("sexEligible", "ALL");
+	// formData.append("acceptedHealthy", "YES");
+	// formData.append("samplingMethod", null);
+	// formData.append("observationalModel", null);
+	// formData.append("timePerspective", null);
+	// formData.append("interventionTreatment", null);
+	// formData.append("primaryOutcome", null);
+	// formData.append("secondaryOutcome", null);
+	// formData.append("pi", null);
+	// formData.append("collaborators", null);
+	// formData.append("publications", null);
 
-  const testJson = {
-    projectId: "2.1",
-    status: "ACTIVE",
-    projectTitle:
-      "Real-time Decision Support by Light-weighted AI Model Trained with Large-scale Data for Breast Cancer 30",
-    startDate: "2023-05-22",
-    completeDate: "2025-05-22",
-    enrollment: 2000,
-    studyType: "CHAT_AI",
-    otherStudyId: null,
-    overview: "Lorem ipsum dolor sit amet, cosectetuer adipiscing elit, ",
-    officialTitle: null,
-    conditions: "Myocardial",
-    name: null,
-    phoneNumber: null,
-    email: null,
-    location: "Bukgu, Daegu, Korea",
-    inclusionCriteria: null,
-    exclusionCriteria: null,
-    ageEligible: null,
-    sexEligible: "ALL",
-    acceptedHealthy: "NO",
-    samplingMethod: null,
-    observationalModel: null,
-    timePerspective: null,
-    interventionTreatment: null,
-    primaryOutcome: null,
-    secondaryOutcome: null,
-    pi: null,
-    collaborators: null,
-    publications: null,
-  };
+	const testJson = {
+		projectId: '2.1',
+		status: 'ACTIVE',
+		projectTitle:
+			'Real-time Decision Support by Light-weighted AI Model Trained with Large-scale Data for Breast Cancer 30',
+		startDate: '2023-05-22',
+		completeDate: '2025-05-22',
+		enrollment: 2000,
+		studyType: 'CHAT_AI',
+		otherStudyId: null,
+		overview: 'Lorem ipsum dolor sit amet, cosectetuer adipiscing elit, ',
+		officialTitle: null,
+		conditions: 'Myocardial',
+		name: null,
+		phoneNumber: null,
+		email: null,
+		location: 'Bukgu, Daegu, Korea',
+		inclusionCriteria: null,
+		exclusionCriteria: null,
+		ageEligible: null,
+		sexEligible: 'ALL',
+		acceptedHealthy: 'NO',
+		samplingMethod: null,
+		observationalModel: null,
+		timePerspective: null,
+		interventionTreatment: null,
+		primaryOutcome: null,
+		secondaryOutcome: null,
+		pi: null,
+		collaborators: null,
+		publications: null,
+	};
 
-  data.append("isEnded", "True");
-  // data.append("thumbnail", undefined);
-  data.append("title", "Test Event 2");
-  data.append("startDate", "2023-10-12");
-  data.append("endDate", "2023-10-17");
-  data.append("isAllDay", "True");
-  data.append("venue", "BeamWorks");
-  data.append("openingHour", "");
-  data.append("relatedWebsite", "");
-  data.append("purpose", "");
-  data.append("explanation", "");
-  // data.append("mainImages", undefined);
-  data.append("location", "");
-  data.append("latitude", "65.454544");
-  data.append("longitude", "77.878974");
-  // data.append("galleries", undefined);
+	data.append('isEnded', 'True');
+	// data.append("thumbnail", undefined);
+	data.append('title', 'Test Event 2');
+	data.append('startDate', '2023-10-12');
+	data.append('endDate', '2023-10-17');
+	data.append('isAllDay', 'True');
+	data.append('venue', 'BeamWorks');
+	data.append('openingHour', '');
+	data.append('relatedWebsite', '');
+	data.append('purpose', '');
+	data.append('explanation', '');
+	// data.append("mainImages", undefined);
+	data.append('location', '');
+	data.append('latitude', '65.454544');
+	data.append('longitude', '77.878974');
+	// data.append("galleries", undefined);
 
-  // const testJson = {
-  //   isEnded: "True",
-  //   thumbnail: undefined,
-  //   title: "Test Event 2",
-  //   startDate: "2023-10-12",
-  //   endDate: "2023-10-17",
-  //   isAllDay: "True",
-  //   venue: "",
-  //   openingHour: "",
-  //   relatedWebsite: "",
-  //   purpose: "",
-  //   explanation: "",
-  //   mainImages: undefined,
-  //   location: "",
-  //   latitude: "65.454544",
-  //   longitude: "77.878974",
-  //   galleries: undefined,
-  // };
+	// const testJson = {
+	//   isEnded: "True",
+	//   thumbnail: undefined,
+	//   title: "Test Event 2",
+	//   startDate: "2023-10-12",
+	//   endDate: "2023-10-17",
+	//   isAllDay: "True",
+	//   venue: "",
+	//   openingHour: "",
+	//   relatedWebsite: "",
+	//   purpose: "",
+	//   explanation: "",
+	//   mainImages: undefined,
+	//   location: "",
+	//   latitude: "65.454544",
+	//   longitude: "77.878974",
+	//   galleries: undefined,
+	// };
 
-  console.log(data);
+	console.log(data);
 
-  JSON.stringify(testJson);
+	JSON.stringify(testJson);
 
-  console.log(testJson);
+	console.log(testJson);
 
-  fetch("http://43.202.46.227/api/projects", {
-    method: "POST",
-    body: testJson,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => res.json().then((data) => console.log("data", data)));
+	fetch('http://43.202.46.227/api/projects', {
+		method: 'POST',
+		body: testJson,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then((res) => res.json().then((data) => console.log('data', data)));
 
-  fetch("http://43.202.46.227/api/events", {
-    method: "GET",
-  }).then((res) => res.json().then((data) => console.log("data", data)));
+	fetch('http://43.202.46.227/api/events', {
+		method: 'GET',
+	}).then((res) => res.json().then((data) => console.log('data', data)));
 };
 
 // 프로젝트는 json 직렬화 + header
--- src/pages/Home/containers/HomeContainer.tsx
+++ src/pages/Home/containers/HomeContainer.tsx
@@ -1,10 +1,22 @@
-import React, { ChangeEvent, useCallback, useState } from 'react';
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { getMainProjects, putMainProjects } from 'src/api/projectsAPI';
+import { getMainInsights, putMainInsights } from 'src/api/InsightAPI';
+import {
+	getOrcaBanner,
+	getMainBanner,
+	putOrcaBanner,
+	putMainBanner,
+} from 'src/api/BannerAPI';
 import Home from '../Home';
+import { EChange } from '@typedef/types';
+
 type Props = {};
 
 const HomeContainer = (props: Props) => {
 	const [selectedMain, setSelectedMain] = useState<File | null>(null);
 	const [selectedOrca, setSelectedOrca] = useState<File | null>(null);
+	const [selectedProjects, setSelectedProjects] = useState<string[]>([]);
+	const [selectedInsights, setSelectedInsights] = useState<string[]>([]);
 	const uploadMainHandler = useCallback(
 		(event: ChangeEvent<HTMLInputElement>) => {
 			const file = event.target.files?.[0];
@@ -12,7 +24,7 @@ const HomeContainer = (props: Props) => {
 				setSelectedMain(file);
 			}
 		},
-		[setSelectedMain],
+		[selectedMain],
 	);
 
 	const uploadOrcaHandler = useCallback(
@@ -22,15 +34,79 @@ const HomeContainer = (props: Props) => {
 				setSelectedOrca(file);
 			}
 		},
-		[setSelectedOrca],
+		[selectedOrca],
 	);
+	const onChangeProjects = useCallback(
+		(e: EChange, index: number) => {
+			const updatedList = [...selectedProjects];
+			updatedList[index] = e.target.value;
+			setSelectedProjects(updatedList);
+			console.log(selectedProjects);
+		},
+		[selectedProjects],
+	);
+	const onProjectSubmit = useCallback(() => {
+		putMainProjects(selectedProjects);
+	}, [selectedProjects]);
+	const onChangeInsights = useCallback(
+		(e: EChange, index: number) => {
+			const updatedList = [...selectedInsights];
+			updatedList[index] = e.target.value;
+			setSelectedInsights(updatedList);
+			console.log(selectedInsights);
+		},
+		[selectedInsights],
+	);
+	const onOrcaBannerSubmit = useCallback(() => {
+		putOrcaBanner(selectedMain, selectedOrca);
+	}, [selectedOrca]);
+	const onMainBannerSubmit = useCallback(() => {
+		putMainBanner(selectedMain, selectedOrca);
+	}, [selectedMain]);
+	const onInsightSubmit = useCallback(() => {
+		putMainInsights(selectedInsights);
+	}, [selectedInsights]);
+	useEffect(() => {
+		getMainProjects().then((data) => {
+			console.log(data.data); // 나옴
+			const updatedList: string[] = [];
+			data.data.map((d: any) => {
+				updatedList.push(`http://orca.beamworks.co.kr/projects/${d.id}`);
+			});
+			setSelectedProjects(updatedList);
+		});
+		getMainInsights().then((data) => {
+			console.log(data.data); // 나옴
+			const updatedList: string[] = [];
+			data.data.map((d: any) => {
+				updatedList.push(`http://orca.beamworks.co.kr/insights/${d.id}`);
+			});
+			setSelectedInsights(updatedList);
+		});
+		getOrcaBanner().then((data) => {
+			console.log(data.data); // 나옴
+		});
+		getMainBanner().then((data) => {
+			// console.log(data.data); // 나옴
+		});
+		return () => {};
+	}, []);
 
+	console.log(selectedInsights);
 	return (
 		<Home
 			selectedMain={selectedMain}
 			selectedOrca={selectedOrca}
 			uploadMainHandler={uploadMainHandler}
 			uploadOrcaHandler={uploadOrcaHandler}
+			selectedProjects={selectedProjects}
+			onChangeProjects={onChangeProjects}
+			onProjectSubmit={onProjectSubmit}
+			selectedInsights={selectedInsights}
+			onChangeInsights={onChangeInsights}
+			onInsightSubmit={onInsightSubmit}
+			onOrcaBannerSubmit={onOrcaBannerSubmit}
+			onMainBannerSubmit={onMainBannerSubmit}
 		/>
 	);
 };
